141010fd3acc9d18d037c95c97cee377cbc0340b
Andini Afriyanti Lestari <andiniaaf@gmail.com>
2020-05-11 00:03:59 +0700

    UI Login 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,65 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-  home: new HalamanLogin(),
+    home: new HalamanLogin(),
   ));
 }
 
-class HalamanLogin extends StatelessWidget{
-@override
-  Widget build(BuildContext context){
+class HalamanLogin extends StatelessWidget {
+  String imageLink =
+      "https://miro.medium.com/max/3140/1*G59SYlc38Bu39GlqcJ5R6Q.jpeg";
+  @override
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Colors.blue[300],
         title: new Text("Belajar Flutter"),
-       ),
-        body: Padding(
+      ),
+      body: Padding(
         padding: const EdgeInsets.all(50.0),
         child: Form(
           child: Column(
-            children: <Widget>[ 
-               TextFormField(
+            children: <Widget>[
+              Container(
+                child: Image.network(
+                  imageLink,
+                ),
+              ),
+              TextFormField(
                 decoration: InputDecoration(
                   hintText: "Username",
                   contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32.0)),
                 ),
-                ),
-                SizedBox(height:10.0,),
-                TextFormField(
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
                 obscureText: true,
                 decoration: InputDecoration(
                   hintText: "Password",
                   contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32.0)),
                 ),
-                ),
-                MaterialButton(
-                child: Text("bfadlfjladskjfkjaslfjl;asdfj"),
-                color: Colors.lightBlueAccent,
-                onPressed: () {},
-                ),    
-      ],
+              ),
+              // MaterialButton(
+              //   child: Text("Login"),
+              //   color: Colors.lightBlueAccent,
+              //   onPressed: () {},
+              // ),
+              new OutlineButton(
+                  child: new Text("Login"),
+                  color: Colors.lightBlueAccent,
+                  onPressed: (){},
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)))
+            ],
+          ),
+        ),
       ),
-     ),
-    ),
     );
   }
 }
